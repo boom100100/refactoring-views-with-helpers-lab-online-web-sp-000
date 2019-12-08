8e56996fe734e227efe7b736ccc960891ab5ee76
Bernadette Davis <bsd231@nyu.edu>
2019-12-08 20:40:58 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,8 +2,8 @@ module ArtistsHelper
   def display_artist(artist)
     if artist
       link_to artist, artist_path(artist)
-    else
-      link_to "Add Artist", edit_song_path(@song)
+    #else
+      #link_to "Add Artist", edit_song_path(@song)
     end
 
   end
